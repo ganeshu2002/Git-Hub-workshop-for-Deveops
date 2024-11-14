fa6d75505b2d03acdba8095dc5d3284e0389cbf0
Samarth <samarthgupta2025@gmail.com>
2024-11-14 10:39:05 +0530

    adding testing files

---
+++ Testing.py
@@ -0,0 +1,6 @@
+print("hello deveops engineer !")
+
+"""If i delete this , i can not rollback and all the info. will
+be lost , when it persists , all the info. will not be there
+that's why we use git(vcs)..
+"""
